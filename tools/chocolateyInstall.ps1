aee6aa886b004b591c89df50800e9cf8b78d80dc
Jason Monsorno <JMonsorno@gmail.com>
2014-10-20 14:57:13 -0400

    Initial import

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = 'VBoxGuestAdditions.install' 
+$url = 'http://download.virtualbox.org/virtualbox/4.3.18/VBoxGuestAdditions_4.3.18.iso'
+
+$unzip = Join-Path $env:TEMP VBoxGuestAdditions
+New-Item -Path $unzip -ItemType Directory -Force | Out-Null
+
+$tempDir = Join-Path $env:TEMP "chocolatey\$packageName"
+New-Item -Path $tempDir -ItemType Directory -Force | Out-Null
+$fileFullPath = Join-Path $tempDir "$packageName.iso"
+Get-ChocolateyWebFile `
+    -packageName $packageName `
+    -url $url -url64bit $url `
+    -fileFullPath $fileFullPath
+
+$7zip = "$($env:ProgramFiles)\7-Zip\7z.exe" 
+$process = Start-Process $7zip -ArgumentList "x -o`"$unzip`" -y `"$fileFullPath`"" -Wait -WindowStyle Hidden -PassThru
+try { if (!($process.HasExited)) { Wait-Process $process } } catch { }
+
+Set-Location $unzip
+Push-Location
+Set-Location .\cert
+.\VBoxCertUtil.exe add-trusted-publisher .\oracle-vbox.cer >$null 2>&1
+Pop-Location
+Install-ChocolateyInstallPackage -packageName $packageName -silentArgs '/S' -file 'VBoxWindowsAdditions.exe'
