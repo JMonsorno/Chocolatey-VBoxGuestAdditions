91e385b59a778a27d37d66950e82180d3561002c
Jason Monsorno <JMonsorno@gmail.com>
2014-10-21 07:11:57 +0000

    Automatic Build/Commit of Version 4.3.18

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'VBoxGuestAdditions.install' 
-$url = 'http://download.virtualbox.org/virtualbox/4.3.16/VBoxGuestAdditions_4.3.16.iso'
+$url = 'http://download.virtualbox.org/virtualbox/4.3.18/VBoxGuestAdditions_4.3.18.iso'
 
 $unzip = Join-Path $env:TEMP VBoxGuestAdditions
 New-Item -Path $unzip -ItemType Directory -Force | Out-Null
