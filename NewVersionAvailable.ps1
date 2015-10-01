ff56ce0f8859ecb7d2cdb552e938505b3e8c9650
unknown <JMonsorno@JMonsornoLT.pilotpen.com>
2015-09-30 21:19:32 -0400

    JMonsorno Reverting changes to v4.3.20 and added code to check for next version to iteratively release each version.

--- NewVersionAvailable.ps1
+++ NewVersionAvailable.ps1
@@ -9,11 +9,13 @@ Set-Location $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 $ErrorActionPreference = "Stop"
 
 #Max Version Online
-$maxVersion = $(
-    (New-Object System.Net.WebClient).DownloadString("http://download.virtualbox.org/virtualbox/") -split "`n" |
-    ? {$_ -match "NAME=`"(\d+\.\d+\.\d+)/`""} |
-    % {[version]$Matches[1]} | Measure-Object -Maximum
-).Maximum
+$versions = (New-Object System.Net.WebClient).DownloadString("http://download.virtualbox.org/virtualbox/") -split "`n" |
+    ? {$_ -match "href=`"(\d+\.\d+\.\d+)/`""} |
+    % {[version]$Matches[1]} 
+
+$maxVersion = $($versions | Measure-Object -Maximum).Maximum
+
+
 
 $powershell = Get-Content .\tools\chocolateyInstall.ps1
 $powershell[1] -match "\d+\.\d+\.\d+" | Out-Null
@@ -21,7 +23,7 @@ $powershell[1] -match "\d+\.\d+\.\d+" | Out-Null
 
 if ($currentVersion -lt $maxVersion)
 {
-    $version = $maxVersion.ToString()
+    $version = $($versions |? {$_ -gt $currentVersion}  | Measure-Object -Minimum).Minimum.ToString()
     Send-MailMessage -To $AlertRecipient -Subject "Chocolatey-VBoxGuestAdditions" -Body "Version $version is available" -From $Credentials.UserName -SmtpServer smtp.mandrillapp.com -Port 587 -Credential $Credentials
     $powershell[1] = "`$url = 'http://download.virtualbox.org/virtualbox/$version/VBoxGuestAdditions_$version.iso'"
     Set-Content -Path .\tools\chocolateyInstall.ps1 -Value $powershell
