be735e6c7d908663ad722bd7c5d7b103fe0c7c86
Jason Monsorno <JMonsorno@gmail.com>
2015-10-02 00:46:29 +0000

    Automatic Build/Commit of Version chocInstall Chocolatey v0.9.9.8 Chocolatey is unpacking required files for use. Overwriting? True Creating ChocolateyInstall as an environment variable (targeting 'Machine')   Setting ChocolateyInstall to 'C:\ProgramData\chocolatey' Adding Modify permission for current user to 'C:\ProgramData\chocolatey' We are setting up the Chocolatey package repository. The packages themselves go to 'C:\ProgramData\chocolatey\lib'   (i.e. C:\ProgramData\chocolatey\lib\yourPackageName). A shim file for the command line goes to 'C:\ProgramData\chocolatey\bin'   and points to an executable in 'C:\ProgramData\chocolatey\lib\yourPackageName'.
    Creating Chocolatey folders if they do not already exist.
     Chocolatey v0.9.9.8 Chocolatey is unpacking required files for use. Overwriting? True Ensuring 'C:\ProgramData\chocolatey\lib\chocolatey' exists. Chocolatey (choco.exe) is now ready.
    You can call choco from anywhere, command line or powershell by typing choco.
    Run choco /? for a list of functions.
    You may need to shut down and restart powershell and/or consoles
     first prior to using choco. Chocolatey v0.9.9.8 Attempting to build package from 'VBoxGuestAdditions.nuspec'. Successfully created package 'VBoxGuestAdditions.install.5.0.4.nupkg' 5.0.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'VBoxGuestAdditions.install' 
-$url = 'http://download.virtualbox.org/virtualbox/5.0.2/VBoxGuestAdditions_5.0.2.iso'
+$url = 'http://download.virtualbox.org/virtualbox/5.0.4/VBoxGuestAdditions_5.0.4.iso'
 
 $unzip = Join-Path $env:TEMP VBoxGuestAdditions
 New-Item -Path $unzip -ItemType Directory -Force | Out-Null
