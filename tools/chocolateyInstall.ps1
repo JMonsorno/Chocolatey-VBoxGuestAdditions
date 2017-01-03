d7539c4f2f05e7a1a28d8ecbb6522143feb288d0
Simon Colmer <simon.colmer@totaljobsgroup.com>
2017-01-03 14:16:38 +0000

    Added support for 5.1.12

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
-$packageName = 'VBoxGuestAdditions.install' 
-$url = 'http://download.virtualbox.org/virtualbox/4.3.26/VBoxGuestAdditions_4.3.26.iso'
+$packageName = 'VBoxGuestAdditions.install'
+$url = 'http://download.virtualbox.org/virtualbox/5.1.12/VBoxGuestAdditions_5.1.12.iso'
 
 $unzip = Join-Path $env:TEMP VBoxGuestAdditions
+$certPath = Join-Path $unzip "cert"
 New-Item -Path $unzip -ItemType Directory -Force | Out-Null
 
 $tempDir = Join-Path $env:TEMP "chocolatey\$packageName"
@@ -12,14 +13,21 @@ Get-ChocolateyWebFile `
     -url $url -url64bit $url `
     -fileFullPath $fileFullPath
 
-$7zip = "$($env:ProgramFiles)\7-Zip\7z.exe" 
+$7zip = "$($env:ProgramFiles)\7-Zip\7z.exe"
 $process = Start-Process $7zip -ArgumentList "x -o`"$unzip`" -y `"$fileFullPath`"" -Wait -WindowStyle Hidden -PassThru
 try { if (!($process.HasExited)) { Wait-Process $process } } catch { }
 
 Set-Location $unzip
 Push-Location
 Set-Location .\cert
-.\VBoxCertUtil.exe add-trusted-publisher .\oracle-vbox.cer >$null 2>&1
+
+Write-Output "Installing certs..."
+Get-ChildItem $certPath -Filter *.cer |
+Foreach-Object {
+    $file = ".\" + $_.Name
+    .\VBoxCertUtil.exe add-trusted-publisher $file >$null 2>&1
+}
+
 Pop-Location
 $filename = $(Get-ChildItem .\VBoxWindowsAdditions.exe).FullName
 Install-ChocolateyInstallPackage -packageName $packageName -silentArgs '/S' -file $filename
